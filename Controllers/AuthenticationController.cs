d48929706e4cb07ba2083b2b44d469d4fad5ac3e
Nikita <nameday31@gmail.com>
2023-02-22 17:09:28 -0500

    add AuthenticationController

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeaStoreApi.Context;
+
+namespace TeaStoreApi.Controllers
+{
+    public class AuthenticationController:ControllerBase
+    {
+        private dbcontext db;
+        public AuthenticationController(dbcontext db)
+        {
+            this.db = db;
+        }
+    }
+}
